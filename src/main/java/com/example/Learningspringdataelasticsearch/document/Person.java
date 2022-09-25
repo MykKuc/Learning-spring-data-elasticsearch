a289add6d8b0f60a8d069b69d71b14a034eafced
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-09-25 18:00:20 +0300

    feat: added Person entity and PersonRepository Interface

--- src/main/java/com/example/Learningspringdataelasticsearch/document/Person.java
+++ src/main/java/com/example/Learningspringdataelasticsearch/document/Person.java
@@ -1,8 +1,23 @@
 package com.example.Learningspringdataelasticsearch.document;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+import com.example.Learningspringdataelasticsearch.helper.*;
+
+
+
+@Document(indexName = Indices.PERSON_INDEX)
+@Setting(settingPath = "Learning-spring-data-elasticsearch\\src\\main\\resources\\static\\es-settings.json")
 public class Person {
 
+    @Id
+    @Field(type = FieldType.Keyword)
     private String id;
+
+    @Field(type = FieldType.Text)
     private String name;
 
     public String getId() {
---
+++ src/main/java/com/example/Learningspringdataelasticsearch/helper/Indices.java
@@ -0,0 +1,6 @@
+package com.example.Learningspringdataelasticsearch.helper;
+
+public class Indices {
+
+    public static final String PERSON_INDEX = "person";
+}
