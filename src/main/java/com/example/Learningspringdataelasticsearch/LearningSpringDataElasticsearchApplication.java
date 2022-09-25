c621794f3068f7097b2c372a107acef39a07c988
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-09-25 16:28:14 +0300

    start: initialize the repository and create Configuration class for Elasticsearch

---
+++ src/main/java/com/example/Learningspringdataelasticsearch/LearningSpringDataElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.Learningspringdataelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringDataElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringDataElasticsearchApplication.class, args);
+	}
+
+}
